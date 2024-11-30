d150654a680a3b21d2581670eae81ae09d0892ca
ramzini22 <ramil.zinnatullin.02@mail.ru>
2024-11-30 22:33:07 +0300

    feat: add time on last chat message

--- src/components/chat-item/index.tsx
+++ src/components/chat-item/index.tsx
@@ -6,18 +6,33 @@ import { ChatType } from '../../root/types/chat/chat.type.ts';
 import { useTranslation } from 'react-i18next';
 import { ChatEnum } from '../../root/types/chat/chat.enum.ts';
 import { MessageTypeEnum } from '../../root/types/chat/message-type.enum.ts';
+import moment from 'moment/min/moment-with-locales';
 
 const ChatItem: FC<{ chat: ChatType }> = ({ chat }) => {
     const { t } = useTranslation();
     const navigate = useNavigate();
     const [message, setMessage] = useState<string>();
+    const [time, setTime] = useState<string>();
     const { id } = useParams();
+
+    const updateTime = (data: Date) => {
+        const now = new Date();
+        const correctData = new Date(data);
+
+        const diffAtDays = (now.getTime() - correctData.getTime()) / (1000 * 60 * 60 * 24);
+
+        if (diffAtDays < 1) setTime(moment(correctData).format('LT'));
+        else if (diffAtDays >= 1 && diffAtDays < 7) setTime(moment(correctData).format('dddd'));
+        else setTime(moment(correctData).calendar());
+    };
 
     useEffect(() => {
         if (chat.type === ChatEnum.IS_OPEN) {
             const message = chat.messages[0];
             const visibleMessage = message.type === MessageTypeEnum.IS_SYSTEM ? t('chat_is_create') : message.message;
+
             setMessage(visibleMessage);
+            updateTime(message.createdAt);
         }
     }, [chat]);
 
@@ -31,7 +46,10 @@ const ChatItem: FC<{ chat: ChatType }> = ({ chat }) => {
         >
             <ChatAvatar type={chat.type} />
             <div className={styles.main_inf}>
-                <div className={styles.title}>{chat.title}</div>
+                <div className={styles.title_block}>
+                    <div className={styles.title}>{chat.title}</div>
+                    <div className={styles.time}>{time}</div>
+                </div>
                 <div className={styles.message_block}>
                     <div className={styles.message}>{message}</div>
                     <div className={styles.count_message}>{chat.countMessages}</div>
