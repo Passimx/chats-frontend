6d5e9f08e962ddabc31287422f84b42bb550f3f0
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-03-11 11:01:47 +0300

    fix: hide service worker

--- src/root/wrappers/app/hooks/use-shared-worker.ts
+++ src/root/wrappers/app/hooks/use-shared-worker.ts
@@ -75,7 +75,7 @@ export const useSharedWorker = () => {
     const sendMessage = useNotificationAction();
 
     useEffect(() => {
-        const socket = new WebSocket('ws://api.tons-chat.ru/ws');
+        const socket = new WebSocket('wss://api.tons-chat.ru/ws');
 
         socket.addEventListener('message', (event) => {
             const data = JSON.parse(event.data);
