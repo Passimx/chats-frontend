4d09a2e92909fad9336432991ab5d9245b642f25
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-03-16 11:52:45 +0300

    feat: add ws

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
 import ReactDOM from 'react-dom/client';
 import App from './app.tsx';
 import './index.css';
-import { registerSW } from 'virtual:pwa-register';
+// import { registerSW } from 'virtual:pwa-register';
 
-registerSW({ immediate: true });
+// registerSW({ immediate: true });
 
 // navigator.serviceWorker.getRegistrations().then((registrations) => {
 //     for (const registration of registrations) {
