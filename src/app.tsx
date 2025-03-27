13a6017f8fd7c03b7d3ed71199c5073aceff67ee
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-03-27 13:45:31 +0300

    feat: add tel script

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,15 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import AppRouter from './root/routes/app-router.tsx';
 import { store } from './root/store';
 import { Provider } from 'react-redux';
 
 const App: FC = () => {
+    useEffect(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-expect-error
+        console.log(window?.Telegram?.WebApp);
+    }, []);
+
     return (
         <Provider store={store}>
             <AppRouter />
