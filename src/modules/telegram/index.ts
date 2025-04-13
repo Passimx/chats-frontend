dda97e1c824dea418d580b04459f7bfcb08b093c
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-13 13:44:24 +0300

    feat: correct read message

--- src/modules/telegram/index.ts
+++ src/modules/telegram/index.ts
@@ -1,7 +1,6 @@
 import { useOpen } from './hooks/use-open.hook.ts';
-import { useSaveHeight } from './hooks/use-save-height.hook.ts';
 
 export const useTelegram = () => {
     useOpen();
-    useSaveHeight();
+    // useSaveHeight();
 };
