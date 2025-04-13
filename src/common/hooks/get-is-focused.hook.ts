e37f817e010dc28c0fabb2e1b2d73e949184a24e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-13 15:57:22 +0300

    refactor: correct select emodji

--- src/common/hooks/get-is-focused.hook.ts
+++ src/common/hooks/get-is-focused.hook.ts
@@ -1,7 +1,12 @@
-export const getIsFocused = (element: HTMLElement) => {
-    const selection = window.getSelection();
-    if (!selection || selection.rangeCount === 0) return false;
+export const getIsFocused = (element: HTMLElement): boolean => {
+    const elFocused = document.activeElement === element || element.contains(document.activeElement);
 
-    const range = selection.getRangeAt(0);
-    return element.contains(range.startContainer);
+    try {
+        const sel = window.getSelection();
+        const caretInside = sel && sel.rangeCount > 0 ? element.contains(sel.getRangeAt(0).startContainer) : false;
+
+        return elFocused || caretInside;
+    } catch (e) {
+        return elFocused;
+    }
 };
