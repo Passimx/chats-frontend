9e91df8c8ce0ea6573ab2cbba07c48bebb2763be
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-14 22:01:21 +0300

    feat: correct reconnect socket

--- src/components/input-message/hooks/use-enter.hook.ts
+++ src/components/input-message/hooks/use-enter.hook.ts
@@ -17,9 +17,10 @@ export const useEnterHook = (): UseEnterHookType => {
     const { isPhone, isOpenMobileKeyboard } = useAppSelector((state) => state.app);
 
     const placeholder = useMemo((): string => {
+        return String(isOpenMobileKeyboard);
         const text = chatOnPage?.type === ChatEnum.IS_SYSTEM ? 'chats_message_unavailable' : 'chats_enter_message';
         return t(text);
-    }, [chatOnPage?.type, t]);
+    }, [chatOnPage?.type, t, isOpenMobileKeyboard]);
 
     const onInput = useCallback(() => {
         const el = document.getElementById(styles.new_message)!;
