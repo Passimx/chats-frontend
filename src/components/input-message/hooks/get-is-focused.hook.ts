8b0e73536a81ce1f38572875cddf243db7333ba2
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-14 18:51:55 +0300

    feat: add ping pong logic

--- src/components/input-message/hooks/get-is-focused.hook.ts
+++ src/components/input-message/hooks/get-is-focused.hook.ts
@@ -3,5 +3,9 @@ import styles from '../index.module.css';
 export const getIsFocused = (): boolean => {
     const element = document.getElementById(styles.new_message)!;
 
-    return document.activeElement === element;
+    const selection = window.getSelection();
+    if (!selection || selection.rangeCount === 0) return false;
+
+    const range = selection.getRangeAt(0);
+    return element.contains(range.startContainer);
 };
