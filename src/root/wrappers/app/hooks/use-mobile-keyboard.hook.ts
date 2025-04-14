bed4875a7e062a55d5c354f54cf757a40f2fdc2e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-14 22:16:53 +0300

    feat: correct reconnect socket

--- src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
+++ src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
@@ -5,24 +5,27 @@ export const useMobileKeyboard = () => {
     const { setStateApp } = useAppAction();
     const { isPhone } = useAppSelector((state) => state.app);
 
-    useEffect(() => {
-        if (!isPhone) return;
-
-        const handler = () => {
-            const height = window.visualViewport?.height;
-            if (!height) return;
-
-            setTimeout(() => {
-                alert(JSON.stringify([height, window.innerHeight]));
-            }, 5000);
-
-            if (height < window.innerHeight - 100) setStateApp({ isOpenMobileKeyboard: true });
-            else setStateApp({ isOpenMobileKeyboard: false });
-        };
-
-        window.visualViewport?.addEventListener('resize', handler);
-        return () => window.visualViewport?.removeEventListener('resize', handler);
-    }, [isPhone]);
+    // useEffect(() => {
+    //     if (!isPhone) return;
+    //
+    //     const handler = () => {
+    //         const height = window.visualViewport?.height;
+    //         if (!height) return;
+    //
+    //         setTimeout(() => {
+    //             alert(JSON.stringify([height, window.innerHeight]));
+    //         }, 5000);
+    //         // 413 413
+    //         // 413 436
+    //         // 749 793
+    //         // 793 793
+    //         if (height < window.innerHeight - 100) setStateApp({ isOpenMobileKeyboard: true });
+    //         else setStateApp({ isOpenMobileKeyboard: false });
+    //     };
+    //
+    //     window.visualViewport?.addEventListener('resize', handler);
+    //     return () => window.visualViewport?.removeEventListener('resize', handler);
+    // }, [isPhone]);
 
     useEffect(() => {
         if (!isPhone) return;
