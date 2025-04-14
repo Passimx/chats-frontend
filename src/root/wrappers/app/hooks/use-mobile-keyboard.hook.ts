cf974d2b4d31c69de07408ffd4f305ed05f6806e
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-14 22:09:34 +0300

    feat: correct reconnect socket

--- src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
+++ src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
@@ -12,6 +12,10 @@ export const useMobileKeyboard = () => {
             const height = window.visualViewport?.height;
             if (!height) return;
 
+            setTimeout(() => {
+                alert(JSON.stringify([height, window.innerHeight]));
+            }, 5000);
+
             if (height < window.innerHeight - 100) setStateApp({ isOpenMobileKeyboard: true });
             else setStateApp({ isOpenMobileKeyboard: false });
         };
@@ -19,4 +23,22 @@ export const useMobileKeyboard = () => {
         window.visualViewport?.addEventListener('resize', handler);
         return () => window.visualViewport?.removeEventListener('resize', handler);
     }, [isPhone]);
+
+    useEffect(() => {
+        const lockScroll = () => {
+            setStateApp({ isOpenMobileKeyboard: true });
+        };
+
+        const unlockScroll = () => {
+            setStateApp({ isOpenMobileKeyboard: false });
+        };
+
+        window.addEventListener('focusin', lockScroll); // iOS Safari когда клавиатура появляется
+        window.addEventListener('focusout', unlockScroll); // когда скрывается
+
+        return () => {
+            window.removeEventListener('focusin', lockScroll);
+            window.removeEventListener('focusout', unlockScroll);
+        };
+    }, []);
 };
