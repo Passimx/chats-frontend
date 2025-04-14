5250b0dd3426ba47c455396b8c7492f9f067e6d3
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-04-14 22:49:31 +0300

    feat: correct reconnect socket

--- src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
+++ src/root/wrappers/app/hooks/use-mobile-keyboard.hook.ts
@@ -8,34 +8,20 @@ export const useMobileKeyboard = () => {
     useEffect(() => {
         if (!isPhone) return;
 
-        const handler = () => {
-            const height = window.visualViewport?.height;
-            if (!height) return;
-
-            setTimeout(() => setStateApp({ isOpenMobileKeyboard: height < window.innerHeight - 100 }), 300);
+        const lockScroll = () => {
+            setTimeout(() => setStateApp({ isOpenMobileKeyboard: true }), 300);
         };
 
-        window.visualViewport?.addEventListener('resize', handler);
-        return () => window.visualViewport?.removeEventListener('resize', handler);
-    }, [isPhone]);
+        const unlockScroll = () => {
+            setTimeout(() => setStateApp({ isOpenMobileKeyboard: false }), 300);
+        };
 
-    // useEffect(() => {
-    //     if (!isPhone) return;
-    //
-    //     const lockScroll = () => {
-    //         setTimeout(() => setStateApp({ isOpenMobileKeyboard: true }), 300);
-    //     };
-    //
-    //     const unlockScroll = () => {
-    //         setTimeout(() => setStateApp({ isOpenMobileKeyboard: false }), 300);
-    //     };
-    //
-    //     window.addEventListener('focusin', lockScroll); // iOS Safari когда клавиатура появляется
-    //     window.addEventListener('focusout', unlockScroll); // когда скрывается
-    //
-    //     return () => {
-    //         window.removeEventListener('focusin', lockScroll);
-    //         window.removeEventListener('focusout', unlockScroll);
-    //     };
-    // }, [isPhone]);
+        window.addEventListener('focusin', lockScroll); // iOS Safari когда клавиатура появляется
+        window.addEventListener('focusout', unlockScroll); // когда скрывается
+
+        return () => {
+            window.removeEventListener('focusin', lockScroll);
+            window.removeEventListener('focusout', unlockScroll);
+        };
+    }, [isPhone]);
 };
