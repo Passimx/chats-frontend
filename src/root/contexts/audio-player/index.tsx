c278201b5fef7086e1dd8205b2ff8ea6bea637e1
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-09-14 19:10:37 +0300

    refactor: change player

--- src/root/contexts/audio-player/index.tsx
+++ src/root/contexts/audio-player/index.tsx
@@ -1,60 +1,92 @@
-import { createContext, FC, memo, ReactElement, useCallback, useState } from 'react';
+import { createContext, FC, memo, ReactElement, useCallback, useEffect, useState } from 'react';
 import { AudioType, ContextType } from './types/context.type.ts';
+import { useFileSize } from '../../../common/hooks/use-file-size.ts';
 
 export const AudioPlayerContext = createContext<ContextType | null>(null);
-let audioInside: AudioType;
-let context: AudioBufferSourceNode;
-let offset: number;
+// let audioInside: AudioType;
+// let context: AudioBufferSourceNode;
+
+let audioEl: HTMLAudioElement | null = null;
 
 export const AudioPlayer: FC<{ children: ReactElement }> = memo(({ children }) => {
     const [audio, setAudio] = useState<AudioType>();
     const [isPlaying, setIsPlaying] = useState<boolean>(false);
+    const getFileSize = useFileSize;
 
-    const play: () => Promise<void> = () =>
-        // eslint-disable-next-line no-async-promise-executor
-        new Promise(async (resolve) => {
-            const fileId = audioInside.file.id;
-            if (context) context.stop();
-            if (!audioInside?.blob) return resolve();
+    const play = async () => {
+        setIsPlaying(true);
+    };
 
-            const arrayBuffer = await audioInside.blob.arrayBuffer();
-            const audioContext = new window.AudioContext();
-            const source = audioContext.createBufferSource();
+    const pause = useCallback(() => setIsPlaying(false), []);
 
-            source.buffer = await audioContext.decodeAudioData(arrayBuffer);
-            source.connect(audioContext.destination);
-
-            if (source.buffer?.duration - 0.1 < offset) offset = 0;
-            source.start(0, offset);
-            setIsPlaying(true);
-            const begin = Date.now() - (offset ?? 0) * 1000;
-            context = source;
-
-            context.onended = () => {
-                const end = Date.now();
-                const currentTime = context.context.currentTime;
-                const duration = source.buffer?.duration ?? 0 - 0.1;
-
-                if ((duration && currentTime > duration) || fileId !== audioInside?.file?.id) {
-                    setIsPlaying(false);
-                    offset = 0;
-                } else offset = (end - begin) / 1000;
-
-                resolve();
-            };
-        });
-
-    const pause = useCallback(() => {
-        if (!context) return;
-        context.stop();
-        setIsPlaying(false);
+    const timeupdate = useCallback(() => {
+        //     const current = audioEl.currentTime; // сколько секунд прошло
+        //     const total = audioEl.duration; // общая длительность
+        //
+        //     const progress = (current / total) * 100; // прогресс в процентах
+        //     console.log('Прогресс:', progress);
     }, []);
 
-    const addFile = useCallback((value: AudioType) => {
-        if (audio?.file?.id !== value?.file?.id) offset = 0;
-        audioInside = value;
-        setAudio(value);
-    }, []);
+    const addFile = useCallback(
+        (value: AudioType) => {
+            if (value.file.id && value.file.id !== audio?.file.id) {
+                audioEl?.pause();
+                audioEl?.removeEventListener('ended', pause);
+                audioEl?.removeEventListener('timeupdate', timeupdate);
+                audioEl = null;
+
+                const url = URL.createObjectURL(value.blob);
+                audioEl = new Audio(url);
+
+                audioEl.addEventListener('ended', pause);
+                audioEl.addEventListener('timeupdate', timeupdate);
+
+                if ('mediaSession' in navigator) {
+                    const size = getFileSize(audio?.file?.size);
+
+                    navigator.mediaSession.metadata = new MediaMetadata({
+                        title: audio?.file.originalName,
+                        artist: size,
+                        artwork: [{ src: '/assets/icons/512.png', sizes: '512x512', type: 'image/png' }],
+                    });
+
+                    navigator.mediaSession.setActionHandler('play', () => audioEl?.play());
+                    navigator.mediaSession.setActionHandler('pause', () => audioEl?.pause());
+
+                    navigator.mediaSession.setActionHandler('seekto', (details) => {
+                        if (audioEl && details.seekTime != null) {
+                            audioEl.currentTime = details.seekTime;
+                        }
+                    });
+
+                    navigator.mediaSession.setActionHandler('previoustrack', () => {
+                        if (audioEl) audioEl.currentTime = 0;
+
+                        console.log('⬅️ предыдущая');
+                        // switchToPreviousTrack();
+                    });
+
+                    navigator.mediaSession.setActionHandler('nexttrack', () => {
+                        console.log('➡️ следующая');
+                        // switchToNextTrack();
+                    });
+                }
+            }
+
+            setAudio(value);
+        },
+        [audio],
+    );
+
+    useEffect(() => {
+        if (isPlaying && audio) {
+            audioEl?.play();
+        }
+
+        if (!isPlaying && audio) {
+            audioEl?.pause();
+        }
+    }, [isPlaying, audio]);
 
     return (
         <AudioPlayerContext.Provider value={{ audio, isPlaying, addFile, play, pause }}>
