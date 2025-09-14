902f3135863b7b2098eed752019351eb3aaa51cf
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-09-14 19:45:56 +0300

    refactor: change player

--- src/root/wrappers/app/hooks/use-broadcast-channel.ts
+++ src/root/wrappers/app/hooks/use-broadcast-channel.ts
@@ -38,7 +38,7 @@ export const useBroadcastChannel = () => {
 
         function becomeOwner() {
             const iframe = document.createElement('iframe');
-            iframe.src = 'assets/iframe.html';
+            iframe.src = 'iframe.html';
             iframe.style.display = 'none';
             rawApp.isMainTab = true;
             document.body.appendChild(iframe);
