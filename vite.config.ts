286a3a22defc57a01eff1961097641fcc8d3d598
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-09-14 19:42:13 +0300

    refactor: change player

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
         rollupOptions: {
             input: {
                 main: resolve(__dirname, 'index.html'), // основное SPA
+                iframe: resolve(__dirname, 'iframe.html'), // дополнительная точка входа
             },
         },
         outDir: 'dist',
