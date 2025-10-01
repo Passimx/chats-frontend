8f2af194e15c5bf9ea6132ca2e7b19ef4318b97b
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-10-01 17:16:56 +0800

    feat: add choose files at mobile

--- src/components/media-menu/index.tsx
+++ src/components/media-menu/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC, memo, useEffect } from 'react';
 import styles from './index.module.css';
 import { GoFileMedia } from 'react-icons/go';
 import { CiFileOn } from 'react-icons/ci';
@@ -6,14 +6,12 @@ import { useOpenMedia } from './hooks/use-open-media.hook.ts';
 import { PropsType } from './types.ts';
 import useVisibility from '../../common/hooks/use-visibility.ts';
 import useClickOutside from '../../common/hooks/use-click-outside.ts';
-import { useAppSelector } from '../../root/store';
 import { useTranslation } from 'react-i18next';
 
-export const MediaMenu: FC<PropsType> = ({ isVisibleMediaMenuOutside, setIsVisibleMediaMenuOutside }) => {
+export const MediaMenu: FC<PropsType> = memo(({ isVisibleMediaMenuOutside, setIsVisibleMediaMenuOutside }) => {
     const visibility = useVisibility;
     const [wrapperRef, isVisible, setIsVisible] = useClickOutside();
     const [openMedia, openFiles] = useOpenMedia(setIsVisible);
-    const { isPhone } = useAppSelector((state) => state.app);
     const { t } = useTranslation();
 
     useEffect(() => {
@@ -35,16 +33,14 @@ export const MediaMenu: FC<PropsType> = ({ isVisibleMediaMenuOutside, setIsVisib
                 <GoFileMedia className={styles.menu_item_icon} />
                 <div className="text_translate">{t('media')}</div>
             </div>
-            {!isPhone && (
-                <div className={styles.menu_item} onClick={openFiles}>
-                    <CiFileOn className={styles.menu_item_icon} strokeWidth={1} />
-                    <div className="text_translate">{t('file')}</div>
-                </div>
-            )}
+            <div className={styles.menu_item} onClick={openFiles}>
+                <CiFileOn className={styles.menu_item_icon} strokeWidth={1} />
+                <div className="text_translate">{t('file')}</div>
+            </div>
             {/*<div className={styles.menu_item}>*/}
             {/*    <MdOutlineLocationOn className={styles.menu_item_icon} />*/}
             {/*    <div>Геолокация</div>*/}
             {/*</div>*/}
         </div>
     );
-};
+});
