0cc308aaafa13a659bbf72f554956e07becd92b1
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-10-10 22:40:23 +0300

    feat: add new settings

--- src/root/wrappers/app/hooks/use-battery.hook.ts
+++ src/root/wrappers/app/hooks/use-battery.hook.ts
@@ -5,14 +5,15 @@ export const useBattery = () => {
     const { setStateApp } = useAppAction();
 
     useEffect(() => {
-        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-        // @ts-expect-error
-        navigator?.getBattery()?.then((battery) => {
-            setStateApp({ batteryLevel: battery.level * 100 });
-            setStateApp({ batteryCharging: battery.charging });
+        if ('getBattery' in navigator)
+            // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+            // @ts-expect-error
+            navigator?.getBattery()?.then((battery) => {
+                setStateApp({ batteryLevel: battery.level * 100 });
+                setStateApp({ batteryCharging: battery.charging });
 
-            battery.addEventListener('levelchange', () => setStateApp({ batteryLevel: battery.level * 100 }));
-            battery.addEventListener('chargingchange', () => setStateApp({ batteryCharging: battery.charging }));
-        });
+                battery.addEventListener('levelchange', () => setStateApp({ batteryLevel: battery.level * 100 }));
+                battery.addEventListener('chargingchange', () => setStateApp({ batteryCharging: battery.charging }));
+            });
     }, []);
 };
