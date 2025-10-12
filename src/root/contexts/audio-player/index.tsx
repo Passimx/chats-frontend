2791d9cc983374a7f92ab9d221bfcaaf90f7d452
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-10-12 11:49:40 +0300

    feat: correct music picture

--- src/root/contexts/audio-player/index.tsx
+++ src/root/contexts/audio-player/index.tsx
@@ -65,32 +65,14 @@ export const AudioPlayer: FC<{ children: ReactElement }> = memo(({ children }) =
 
                     if (value.file.metadata.artist) artist = value.file.metadata.artist;
 
-                    if (value.file.metadata.previewId) {
-                        const src = `${Envs.filesServiceUrl}/${value.file.chatId}/${value.file.metadata.previewId}`;
+                    if (value.file.metadata.previewId)
                         artwork = [
                             {
-                                src,
-                                sizes: '96x96',
-                                type: value.file.metadata.previewMimeType as string,
-                            },
-                            {
-                                src,
-                                sizes: '128x128',
-                                type: value.file.metadata.previewMimeType as string,
-                            },
-                            {
-                                src,
-                                sizes: '256x256',
-                                type: value.file.metadata.previewMimeType as string,
-                            },
-                            {
-                                src,
+                                src: `${Envs.filesServiceUrl}/${value.file.chatId}/${value.file.metadata.previewId}`,
                                 sizes: '512x512',
                                 type: value.file.metadata.previewMimeType as string,
                             },
                         ];
-                    }
-                    console.log(artwork);
 
                     navigator.mediaSession.metadata = new MediaMetadata({
                         title,
