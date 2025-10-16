b57d664d94cedbcd2ff1135bce36e72e78795770
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-10-16 23:43:38 +0300

    feat: add calculate cache

--- src/root/wrappers/app/index.tsx
+++ src/root/wrappers/app/index.tsx
@@ -26,6 +26,7 @@ import { useBattery } from './hooks/use-battery.hook.ts';
 import { useSettings } from './hooks/use-settings.hook.ts';
 import { useUpdateBadge } from './hooks/use-update-badge.hook.ts';
 import { useVerify } from './hooks/use-verify.hook.ts';
+import { useCatchLogs } from './hooks/use-catch-logs.hook.ts';
 
 const AppWrapper: FC<PropsType> = ({ children }) => {
     // updating chat information
@@ -52,7 +53,7 @@ const AppWrapper: FC<PropsType> = ({ children }) => {
     // update cache files
     useUpdateStaticCache();
     // catch all app log-list
-    // useCatchLogs();
+    useCatchLogs();
     // calculate ios device
     useIsIos();
     // battery Saver Mode
