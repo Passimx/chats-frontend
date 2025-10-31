8c9a66f1a561b13cee445a52c143adfa8d073832
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-10-31 17:05:16 +0300

    feat: add scan qr

--- src/pages/page/index.tsx
+++ src/pages/page/index.tsx
@@ -1,34 +1,41 @@
-import { FC, memo, useEffect } from 'react';
+import { FC, memo, useCallback, useEffect } from 'react';
 import styles from './index.module.css';
 import { useAppAction, useAppSelector } from '../../root/store';
 import { MdOutlineClose } from 'react-icons/md';
 import { setThemeColor } from '../../common/hooks/set-theme-color.ts';
+import useClickOutside from '../../common/hooks/use-click-outside.ts';
 
 export const Page: FC = memo(() => {
-    const page = useAppSelector((state) => state.app.page);
     const { setStateApp } = useAppAction();
+    const [ref, isVisible, setIsVisible] = useClickOutside();
+    const page = useAppSelector((state) => state.app.page);
+
+    const handleKeyDown = useCallback((e: KeyboardEvent) => {
+        if (e.key === 'Escape') setStateApp({ page: undefined });
+    }, []);
 
     useEffect(() => {
-        if (!page) return;
-
-        const handleKeyDown = (e: KeyboardEvent) => {
-            if (e.key === 'Escape') setStateApp({ page: undefined });
-        };
-
-        window.addEventListener('keydown', handleKeyDown);
-        return () => window.removeEventListener('keydown', handleKeyDown);
+        setIsVisible(!!page);
     }, [page]);
 
     useEffect(() => {
-        if (!page) setThemeColor('#062846');
-        else setThemeColor('#02101C');
-    }, [page]);
+        if (isVisible) {
+            setThemeColor('#02101C');
+            window.addEventListener('keydown', handleKeyDown);
+        }
+
+        if (!isVisible) {
+            setThemeColor('#062846');
+            setStateApp({ page: undefined });
+            window.removeEventListener('keydown', handleKeyDown);
+        }
+    }, [isVisible]);
 
     if (page)
         return (
             <div className={styles.background}>
-                {page}
-                <div className={styles.cancel_background} onClick={() => setStateApp({ page: undefined })}>
+                <div ref={ref}>{page}</div>
+                <div className={styles.cancel_background}>
                     <MdOutlineClose className={styles.cancel_button} />
                 </div>
             </div>
