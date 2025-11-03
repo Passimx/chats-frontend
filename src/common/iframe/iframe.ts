58bccadbc2df1c8c48ef8a5db7a0e862c8b895db
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-11-03 15:46:20 +0300

    feat: add dist.sha256

--- src/common/iframe/iframe.ts
+++ src/common/iframe/iframe.ts
@@ -1,6 +1,4 @@
-import { Envs } from '../config/envs/envs.ts';
 import { EventsFromServer } from '../../root/types/events/events-from-server.type.ts';
-import { EventsEnum } from '../../root/types/events/events.enum.ts';
 const channel = new BroadcastChannel('ws-channel');
 
 let socketId: string | undefined;
@@ -12,35 +10,35 @@ let handleCloseSocket: NodeJS.Timeout | undefined;
 function ping() {
     clearTimeout(handleCloseSocket);
     if (ws.readyState == 1) ws?.send(JSON.stringify({ event: 'ping', data: Date.now() }));
-    handlerDisconnect = setTimeout(() => ws?.close(), Envs.waitPong);
+    handlerDisconnect = setTimeout(() => ws?.close(), 4 * 1000);
 }
 
 function connect() {
-    ws = new WebSocket(Envs.notificationsServiceUrl);
+    ws = new WebSocket(import.meta.env.VITE_NOTIFICATIONS_SERVICE_URL);
 
     ws.onopen = ping;
 
     ws.onmessage = (event: MessageEvent<string>) => {
         const payload = JSON.parse(event.data) as EventsFromServer;
-        if (payload.event === EventsEnum.GET_SOCKET_ID) socketId = payload.data.data;
-        else if (payload.event === EventsEnum.PONG) {
+        if (payload.event === 'get_socket_id') socketId = payload.data.data;
+        else if (payload.event === 'pong') {
             clearTimeout(handlerPing);
             clearTimeout(handlerDisconnect);
-            handlerPing = setTimeout(ping, Envs.intervalPing);
+            handlerPing = setTimeout(ping, 4 * 1000);
         }
         channel.postMessage(payload);
     };
 
     ws.onclose = () => {
         socketId = undefined;
-        handleCloseSocket = setTimeout(() => channel.postMessage({ event: EventsEnum.CLOSE_SOCKET }), Envs.waitPong);
+        handleCloseSocket = setTimeout(() => channel.postMessage({ event: 'close_socket' }), 4 * 1000);
         clearTimeout(handlerPing);
         clearTimeout(handlerDisconnect);
         if (navigator.onLine) connect();
     };
 
     ws.onerror = () => {
-        channel.postMessage({ event: EventsEnum.ERROR, data: '[WS2] Disconnected, reconnecting…' });
+        channel.postMessage({ event: 'error', data: '[WS2] Disconnected, reconnecting…' });
         ws.close();
     };
 }
@@ -51,8 +49,7 @@ channel.onmessage = (ev) => {
     const event = ev.data?.event;
     switch (event) {
         case 'create_tab':
-            if (socketId)
-                channel.postMessage({ event: EventsEnum.GET_SOCKET_ID, data: { success: true, data: socketId } });
+            if (socketId) channel.postMessage({ event: 'get_socket_id', data: { success: true, data: socketId } });
             break;
     }
 };
