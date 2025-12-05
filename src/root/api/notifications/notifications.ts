b7c3f04a761e88297312dd2f6ce988d6ee90acc1
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-12-05 16:57:47 +0300

    fix: correct connect ws

--- src/root/api/notifications/notifications.ts
+++ src/root/api/notifications/notifications.ts
@@ -82,10 +82,19 @@ self.addEventListener('online', () => {
 });
 
 connect();
-document.addEventListener('pageshow', () => {
-    console.log('Страница снова стала видимой!');
 
-    if (!ws || ws.readyState !== WebSocket.OPEN) {
-        connect();
+let lastAlive = Date.now();
+
+setInterval(() => {
+    const now = Date.now();
+
+    if (now - lastAlive > 5000) {
+        console.log('iOS PWA woke up');
+
+        if (!ws || ws.readyState !== WebSocket.OPEN) {
+            connect();
+        }
     }
-});
+
+    lastAlive = now;
+}, 2000);
