d3b7e965b813d13ae888e0335b655718efe0aeed
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-12-06 08:52:43 +0300

    refactor: change semantic rules

--- release.config.cjs
+++ release.config.cjs
@@ -2,7 +2,23 @@ module.exports = {
     branches: ['main'],
     tagFormat: '${version}',
     plugins: [
-        '@semantic-release/commit-analyzer',
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                releaseRules: [
+                    { tag: 'breaking', release: 'major' },
+                    { tag: 'feat', release: 'minor' },
+                    { tag: 'fix', release: 'patch' },
+                    { tag: 'refactor', release: 'patch' },
+                    { tag: 'security', release: 'patch' },
+                    { tag: 'style', release: 'patch' },
+                    { tag: 'chore', release: false },
+                    { tag: 'ci', release: false },
+                    { tag: 'docs', release: false },
+                    { tag: 'test', release: false },
+                ],
+            },
+        ],
         '@semantic-release/release-notes-generator',
         ['@semantic-release/changelog', { changelogFile: 'CHANGELOG.md' }],
         ['@semantic-release/npm', { npmPublish: false }],
@@ -14,17 +30,5 @@ module.exports = {
             },
         ],
         ['@semantic-release/github', { assets: ['CHANGELOG.md'] }],
-    ],
-    releaseRules: [
-        { tag: 'breaking', release: 'major' },
-        { tag: 'feat', release: 'minor' },
-        { tag: 'fix', release: 'patch' },
-        { tag: 'refactor', release: 'patch' },
-        { tag: 'security', release: 'patch' },
-        { tag: 'style', release: 'patch' },
-        { tag: 'chore', release: false },
-        { tag: 'ci', release: false },
-        { tag: 'docs', release: false },
-        { tag: 'test', release: false },
     ],
 };
