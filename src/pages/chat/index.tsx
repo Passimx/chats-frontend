9d8eb0a9886dbad6c51c42c29c68fd373301a4b8
Anton Tsaturov <antonotna9@gmail.com>
2025-12-19 17:14:37 +0300

    feat: Добавлена возможность открыть меню по свайпу  (#217)
    * feat: Добавлена возможность открыть меню по свайпу на мобильном устройстве
    
    * refactor: correct hook
    
    * refactor: correct hook
    
    ---------
    
    Co-authored-by: ramzini22 <ramil.zinnatullin.02@mail.ru>

--- src/pages/chat/index.tsx
+++ src/pages/chat/index.tsx
@@ -29,17 +29,19 @@ import { EmptyMessages } from '../../components/empty-messages';
 import { EventsEnum } from '../../root/types/events/events.enum.ts';
 import { useAutoScroll } from './hooks/use-auto-scroll.hook.ts';
 import { useShortText } from '../../common/hooks/use-short-text.hook.ts';
+import { useSwipeBack } from './hooks/use-swipe.hook.ts';
 
 /** Main chat component */
 const Chat: FC = memo(() => {
     useGetChat();
     useJoinChat();
     useAutoScroll();
-    const { t } = useTranslation();
-    const { setStateApp, postMessageToBroadCastChannel } = useAppAction();
     const [addChat, leave, back] = useMethods();
+    useSwipeBack(back);
+    const { t } = useTranslation();
     const [isLoading, showLastMessages] = useMessages();
     const [wrapperRef, isVisible, setIsVisible] = useClickOutside();
+    const { setStateApp, postMessageToBroadCastChannel } = useAppAction();
     const chatOnPage = useAppSelector((state) => state.chats.chatOnPage);
     const shortName = useShortText(chatOnPage?.id);
     const title = useGetChatTitle(chatOnPage);
