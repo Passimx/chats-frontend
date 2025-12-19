3fdbe7fec8b69e54207aaa61e7eea4b85498d82f
Anton Tsaturov <antonotna9@gmail.com>
2025-12-19 23:56:37 +0300

    Fix: исправлена ошибка, при которой язык озвучки текста не изменялся после смены локали

--- src/components/message/hooks/use-speak.hook.ts
+++ src/components/message/hooks/use-speak.hook.ts
@@ -1,8 +1,10 @@
-import { useCallback, useState } from 'react';
-import { getLanguageByText } from '../../../root/wrappers/app/hooks/translations/get-language-by-text.ts';
+import { useCallback, useEffect, useState } from 'react';
+//import { getLanguageByText } from '../../../root/wrappers/app/hooks/translations/get-language-by-text.ts';
+import { useTranslation } from 'react-i18next';
 
 export const useSpeak = (message: string) => {
     const [isSpeaking, setIsSpeaking] = useState(false);
+    const { i18n } = useTranslation();
 
     const handleSpeaking = () => {
         isSpeaking ? pauseSpeak() : speakAloud();
@@ -12,7 +14,7 @@ export const useSpeak = (message: string) => {
         if (!message) return;
 
         const utterance = new SpeechSynthesisUtterance(message);
-        utterance.lang = getLanguageByText(message);
+        utterance.lang = i18n.language;
         utterance.pitch = 1;
         utterance.rate = 0.9;
 
@@ -21,11 +23,17 @@ export const useSpeak = (message: string) => {
         utterance.onerror = () => setIsSpeaking(false);
 
         window.speechSynthesis.speak(utterance);
-    }, []);
+    }, [message, i18n.language]);
 
     const pauseSpeak = useCallback(() => {
         window.speechSynthesis.cancel();
         setIsSpeaking(false);
+    }, []);
+
+    useEffect(() => {
+        return () => {
+            window.speechSynthesis.cancel();
+        };
     }, []);
 
     return { handleSpeaking, isSpeaking };
