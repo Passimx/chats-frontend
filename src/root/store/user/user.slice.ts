595256e1397c46fc71bba81ab1da58c5fed5b218
NatalyaSA <sin.natalya.19@mail.ru>
2025-12-21 22:41:56 +0300

    Add logout logic

--- src/root/store/user/user.slice.ts
+++ src/root/store/user/user.slice.ts
@@ -1,6 +1,7 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
-import { upsertAccountIndexDb } from './index-db/hooks.ts';
+import { upsertAccountIndexDb, deleteAccountIndexDb } from './index-db/hooks.ts';
 import { UserIndexDbType } from '../../types/users/user-index-db.type.ts';
+import { Envs } from '../../../common/config/envs/envs.ts';
 
 const initialState: Partial<UserIndexDbType> = {};
 
@@ -16,6 +17,14 @@ const UserSlice = createSlice({
                 state[key] = value as never;
             }
             upsertAccountIndexDb(state);
+        },
+        logout(state) {
+            state.key && deleteAccountIndexDb(state.key);
+            state = {};
+            Envs.RSAKeys = undefined;
+            localStorage.removeItem('keys');
+            state.id = undefined;
+            // window.location.reload();
         },
     },
 });
