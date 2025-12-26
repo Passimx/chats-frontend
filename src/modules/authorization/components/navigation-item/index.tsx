a3ca8c46186e48638663cd25afe26cf65e85d0a5
ramzini22 <ramil.zinnatullin.02@mail.ru>
2025-12-26 21:23:51 +0300

    refactor: change start page animation

--- src/modules/authorization/components/navigation-item/index.tsx
+++ src/modules/authorization/components/navigation-item/index.tsx
@@ -1,38 +1,42 @@
-import { FC, memo, useCallback, useMemo } from 'react';
+import { FC, memo, useEffect, useState } from 'react';
 import styles from './index.module.css';
 import styles2 from '../../index.module.css';
-import { IoArrowBack } from 'react-icons/io5';
 import { useAppAction, useAppSelector } from '../../../../root/store';
 import { TabEnum } from '../../../../root/store/app/types/state.type.ts';
+import { useVisibility } from '../../../../common/hooks/use-visibility.hook.ts';
 
 export const NavigationItem: FC<{ children: JSX.Element; index: number }> = memo(({ children, index }) => {
-    const { setStateApp } = useAppAction();
     const id = `navigation_item_${index}`;
+    const { setStateApp } = useAppAction();
+    const [observerTarget, visible] = useVisibility();
+    const [isActive, setIsActive] = useState<boolean>(false);
     const pages = useAppSelector((state) => state.app.pages)?.get(TabEnum.AUTHORIZATION);
 
-    const [width, height] = useMemo(() => {
-        const [element] = document.getElementsByClassName(styles2.page);
-        return [element.clientWidth, element.clientHeight - 32];
-    }, []);
+    useEffect(() => {
+        if (visible) setIsActive(true);
+    }, [visible]);
 
-    const closePage = useCallback(() => {
-        const element = document.getElementById(id)!;
-        element.classList.add(styles.hide_slowly);
+    useEffect(() => {
+        if (!pages) return;
+        if (visible) return;
+        if (!isActive) return;
 
-        setTimeout(() => {
-            if (!pages) return;
-            pages.splice(index, 1);
-            setStateApp({ pages: new Map<TabEnum, JSX.Element[]>([[TabEnum.AUTHORIZATION, pages]]) });
-        }, 200);
+        const element = document.getElementById(styles2.page)!;
+        const activePageIndex = element.scrollLeft / element.clientWidth;
+        if (activePageIndex >= index) return;
+
+        pages.splice(index, 1);
+        setStateApp({ pages: new Map<TabEnum, JSX.Element[]>([[TabEnum.AUTHORIZATION, pages]]) });
+    }, [index, pages, visible, isActive]);
+
+    useEffect(() => {
+        if (!pages?.length) return;
+        const element = document.getElementById(styles2.page)!;
+        element.scrollTo({ left: element.clientWidth * pages.length, behavior: 'smooth' });
     }, [pages]);
 
     return (
-        <div id={id} className={`${styles.background} ${index > 0 && styles.show_slowly}`} style={{ width, height }}>
-            {index > 0 && (
-                <div className={styles.arrow} onClick={closePage}>
-                    <IoArrowBack size={20} />
-                </div>
-            )}
+        <div id={id} ref={observerTarget} className={styles.background}>
             {children}
         </div>
     );
