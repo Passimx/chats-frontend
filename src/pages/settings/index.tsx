7c089577c2e34399a9448830001a0bdee06242dc
NatalyaSA <sin.natalya.19@mail.ru>
2026-02-02 23:06:17 +0300

    bug: fix scrolling for the entire length of the settings menu (#261)
    * bug: fix scrolling for the entire length of the settings menu
    
    * fix: add bottom padding

--- src/pages/settings/index.tsx
+++ src/pages/settings/index.tsx
@@ -24,6 +24,7 @@ import { LuStar } from 'react-icons/lu';
 import { UserInf } from '../../components/user-inf';
 import { Appearance } from '../../components/appearance';
 import { LogOut } from '../../components/log-out/index.tsx';
+import { MenuPadding } from '../../components/menu/compenents/menu-padding/index.tsx';
 
 export const Settings = memo(() => {
     const { t } = useTranslation();
@@ -53,122 +54,126 @@ export const Settings = memo(() => {
         <div id={styles.background}>
             <MenuTitle icon={<IoSettingsOutline />} title={'settings'} />
 
-            <div className={styles.items}>
-                <UserInf />
-            </div>
+            <div id={styles.menu}>
+                <div className={styles.items}>
+                    <UserInf />
+                </div>
 
-            {/*<div className={styles.items}>*/}
-            {/*    <div className={styles.item} onClick={() => selectMenu(<Privacy />)}>*/}
-            {/*        <RiShieldKeyholeLine className={styles.item_logo} />*/}
-            {/*        <div className="text_translate">{t('privacy_policy')}</div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
+                {/*<div className={styles.items}>*/}
+                {/*    <div className={styles.item} onClick={() => selectMenu(<Privacy />)}>*/}
+                {/*        <RiShieldKeyholeLine className={styles.item_logo} />*/}
+                {/*        <div className="text_translate">{t('privacy_policy')}</div>*/}
+                {/*    </div>*/}
+                {/*</div>*/}
 
-            {/*<div className={styles.items}>*/}
-            {/*    <div className={styles.item}>*/}
-            {/*        <IoWalletOutline className={styles.item_logo} />*/}
-            {/*        <div className="text_translate">*/}
-            {/*            {t('wallet')} (<div className={styles.item_value}>6K $</div>)*/}
-            {/*        </div>*/}
-            {/*    </div>*/}
-            {/*</div>*/}
-
-            <div className={styles.items}>
-                <div className={styles.item} onClick={() => selectMenu(<Chats />)}>
-                    <IoChatboxEllipsesOutline className={styles.item_logo} />
-                    <div className="text_translate">
-                        {t('chats')}
-                        <div className={styles.item_value}>&nbsp;{chatsLength}</div>
-                    </div>
-                </div>
-                <div
-                    className={styles.item}
-                    onClick={() => {
-                        if (userName) navigate(userName);
-                    }}
-                >
-                    <LuStar className={styles.item_logo} />
-                    <div className="text_translate">{t('favorites')}</div>
-                </div>
-                <div className={styles.item} onClick={() => selectMenu(<Appearance />)}>
-                    <TbBackground className={styles.item_logo} />
-                    <div className="text_translate">{t('appearance')}</div>
-                </div>
-                <div className={styles.item} onClick={() => selectMenu(<ChangeLanguage />)}>
-                    <GrLanguage className={styles.item_logo} />
-                    <div className="text_translate">
-                        {t('language')}
-                        <div className={styles.item_value}>&nbsp;{t('language_native')}</div>
-                    </div>
-                </div>
-                <div className={styles.item} onClick={() => selectMenu(<Memory />)}>
-                    <TbDatabase className={styles.item_logo} />
-                    <div className="text_translate">
-                        {t('memory_usage')}
-                        <div className={styles.item_value}>&nbsp;{cache}</div>
-                    </div>
-                </div>
-                {Envs.environment !== EnvironmentEnum.PRODUCTION && (
-                    <div className={styles.item} onClick={() => selectMenu(<LogList />)}>
-                        <TbLogs className={styles.item_logo} />
-                        <div className="text_translate">
-                            {t('app_logs')}
-                            {logs?.length && <div className={styles.item_value}>&nbsp;{logs.length}</div>}
-                        </div>
-                    </div>
-                )}
-                <div className={styles.item}>
-                    <IoWalletOutline className={styles.item_logo} />
-                    <div className="text_translate">{t('wallet')}</div>
-                </div>
-                {/*{batteryLevel && (*/}
-                {/*    <div className={styles.item} onClick={() => selectMenu(<BatterySaver />)}>*/}
-                {/*        <MdBatteryCharging20 className={styles.item_logo} />*/}
+                {/*<div className={styles.items}>*/}
+                {/*    <div className={styles.item}>*/}
+                {/*        <IoWalletOutline className={styles.item_logo} />*/}
                 {/*        <div className="text_translate">*/}
-                {/*            {t('battery_saver_mode')}*/}
-                {/*            {batteryLevel && (*/}
-                {/*                <div className={styles.item_value}>&nbsp;{batterySaverMode ? t('on') : t('off')}</div>*/}
-                {/*            )}*/}
+                {/*            {t('wallet')} (<div className={styles.item_value}>6K $</div>)*/}
                 {/*        </div>*/}
                 {/*    </div>*/}
-                {/*)}*/}
-                {/*{isStandalone && (*/}
-                {/*    <div className={styles.item} onClick={() => selectMenu(<Vpn />)}>*/}
-                {/*        <MdVpnLock className={styles.item_logo} />*/}
-                {/*        <div className="text_translate">{t('vpn')}</div>*/}
-                {/*    </div>*/}
-                {/*)}*/}
-            </div>
+                {/*</div>*/}
 
-            <div className={styles.items}>
-                <div
-                    className={styles.item}
-                    onClick={() =>
-                        window.open('https://github.com/orgs/Passimx/discussions/new?category=general', '_blank')
-                    }
-                >
-                    <AiOutlineQuestionCircle className={styles.item_logo} />
-                    <div className="text_translate">{t('help')}</div>
+                <div className={styles.items}>
+                    <div className={styles.item} onClick={() => selectMenu(<Chats />)}>
+                        <IoChatboxEllipsesOutline className={styles.item_logo} />
+                        <div className="text_translate">
+                            {t('chats')}
+                            <div className={styles.item_value}>&nbsp;{chatsLength}</div>
+                        </div>
+                    </div>
+                    <div
+                        className={styles.item}
+                        onClick={() => {
+                            if (userName) navigate(userName);
+                        }}
+                    >
+                        <LuStar className={styles.item_logo} />
+                        <div className="text_translate">{t('favorites')}</div>
+                    </div>
+                    <div className={styles.item} onClick={() => selectMenu(<Appearance />)}>
+                        <TbBackground className={styles.item_logo} />
+                        <div className="text_translate">{t('appearance')}</div>
+                    </div>
+                    <div className={styles.item} onClick={() => selectMenu(<ChangeLanguage />)}>
+                        <GrLanguage className={styles.item_logo} />
+                        <div className="text_translate">
+                            {t('language')}
+                            <div className={styles.item_value}>&nbsp;{t('language_native')}</div>
+                        </div>
+                    </div>
+                    <div className={styles.item} onClick={() => selectMenu(<Memory />)}>
+                        <TbDatabase className={styles.item_logo} />
+                        <div className="text_translate">
+                            {t('memory_usage')}
+                            <div className={styles.item_value}>&nbsp;{cache}</div>
+                        </div>
+                    </div>
+                    {Envs.environment !== EnvironmentEnum.PRODUCTION && (
+                        <div className={styles.item} onClick={() => selectMenu(<LogList />)}>
+                            <TbLogs className={styles.item_logo} />
+                            <div className="text_translate">
+                                {t('app_logs')}
+                                {logs?.length && <div className={styles.item_value}>&nbsp;{logs.length}</div>}
+                            </div>
+                        </div>
+                    )}
+                    <div className={styles.item}>
+                        <IoWalletOutline className={styles.item_logo} />
+                        <div className="text_translate">{t('wallet')}</div>
+                    </div>
+                    {/*{batteryLevel && (*/}
+                    {/*    <div className={styles.item} onClick={() => selectMenu(<BatterySaver />)}>*/}
+                    {/*        <MdBatteryCharging20 className={styles.item_logo} />*/}
+                    {/*        <div className="text_translate">*/}
+                    {/*            {t('battery_saver_mode')}*/}
+                    {/*            {batteryLevel && (*/}
+                    {/*                <div className={styles.item_value}>&nbsp;{batterySaverMode ? t('on') : t('off')}</div>*/}
+                    {/*            )}*/}
+                    {/*        </div>*/}
+                    {/*    </div>*/}
+                    {/*)}*/}
+                    {/*{isStandalone && (*/}
+                    {/*    <div className={styles.item} onClick={() => selectMenu(<Vpn />)}>*/}
+                    {/*        <MdVpnLock className={styles.item_logo} />*/}
+                    {/*        <div className="text_translate">{t('vpn')}</div>*/}
+                    {/*    </div>*/}
+                    {/*)}*/}
                 </div>
-            </div>
 
-            <div className={styles.items}>
-                <div className={styles.item} onClick={() => systemChatName && navigate(systemChatName)}>
-                    <IoRocketOutline className={styles.item_logo} />
-                    <div className="text_translate">
-                        {t('about_app')}
-                        <div className={`${styles.item_value} text_translate`}>
-                            &nbsp;{`(${t('version')} ${Envs.version})`}
+                <div className={styles.items}>
+                    <div
+                        className={styles.item}
+                        onClick={() =>
+                            window.open('https://github.com/orgs/Passimx/discussions/new?category=general', '_blank')
+                        }
+                    >
+                        <AiOutlineQuestionCircle className={styles.item_logo} />
+                        <div className="text_translate">{t('help')}</div>
+                    </div>
+                </div>
+
+                <div className={styles.items}>
+                    <div className={styles.item} onClick={() => systemChatName && navigate(systemChatName)}>
+                        <IoRocketOutline className={styles.item_logo} />
+                        <div className="text_translate">
+                            {t('about_app')}
+                            <div className={`${styles.item_value} text_translate`}>
+                                &nbsp;{`(${t('version')} ${Envs.version})`}
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div className={styles.items}>
-                <div className={styles.item} onClick={() => setStateApp({ page: <LogOut /> })}>
-                    <IoExitOutline className={styles.item_logo} />
-                    <div className="text_translate">{t('log_out')}</div>
+                <div className={styles.items}>
+                    <div className={styles.item} onClick={() => setStateApp({ page: <LogOut /> })}>
+                        <IoExitOutline className={styles.item_logo} />
+                        <div className="text_translate">{t('log_out')}</div>
+                    </div>
                 </div>
+
+                <MenuPadding />
             </div>
         </div>
     );
